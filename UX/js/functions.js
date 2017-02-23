bc14c6380d870d44ca3a4cbd54f8daf1e11d3fc1
Julio Cesar <julio.cesar@soaexpert.com.br>
2017-02-23 19:01:36 -0300

    Commit de alterações na tela de dashboard

--- UX/js/functions.js
+++ UX/js/functions.js
@@ -1,9 +1,14 @@
 $(document).ready(function (){
     $('.areacadastro').fadeToggle(0);
     $('.login__content').fadeIn(1500);
+    $('#menu').fadeIn(0);
 })
 
 $('#btnEmail').click(function () {
     $('.arealogin--body').fadeToggle(0);
     $('.areacadastro').fadeToggle();
+})
+
+$('#btnMenu').click(function (){
+  $("#menu").animate({width:'toggle'},350);
 })
